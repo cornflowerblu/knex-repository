c347056946a268baf8c3a4b0b4af1721996ef089
Roger Urich <rogerurich@overtime.local>
2022-07-01 14:32:10 -0700

    added graphql

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Hapi from '@hapi/hapi'
 import userRoutes from './api/users';
 import userAcountRoutes from './api/users-accounts';
+import http from 'http'
+import { postgraphile } from 'postgraphile'
 
 
 const init = async () => {
@@ -17,6 +19,20 @@ const init = async () => {
     //Load up the routes
     server.route(userAcountRoutes);
     server.route(userRoutes);
+
+    //Spin up GraphQL
+    http
+    .createServer(
+        postgraphile(
+        process.env.DATABASE_URL || "postgres://postgres:postgrespw@localhost:55000/postgres",
+        "public",
+        {
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+        })
+    )
+    .listen(process.env.PORT || 8000);
 };
 
 process.on('unhandledRejection', (err) => {
